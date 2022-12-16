730aeb12e53a54432982e0461031a944f911c86b
Cleiton Lima <cleiton.limapin@gmail.com>
2022-12-16 14:54:53 -0300

    Added method value_from_datadict in widget

---
+++ tests/forms.py
@@ -0,0 +1,53 @@
+from decimal import Decimal
+
+from django import forms
+
+from decimal_mask.widgets import (
+    DecimalMaskWidget,
+    MoneyMaskWidget,
+    PercentMaskWidget,
+)
+
+
+class TestForm(forms.Form):
+    decimal1 = forms.DecimalField(
+        label="Decimal (en-US)",
+        widget=DecimalMaskWidget(),
+    )
+    decimal2 = forms.DecimalField(
+        label="Decimal (pt-BR)",
+        widget=DecimalMaskWidget(
+            decimal_attrs={
+                "locales": "pt-BR",
+                "decimalPlaces": 3,
+            },
+        ),
+    )
+    money1 = forms.DecimalField(
+        label="Money (pt-BR)",
+        widget=MoneyMaskWidget(
+            decimal_attrs={
+                "locales": "pt-BR",
+                "format": {
+                    "style": "currency",
+                    "currency": "BRL",
+                },
+            },
+        ),
+        required=False,
+        help_text="required=False",
+    )
+    money2 = forms.DecimalField(
+        label="Money (widget)",
+        widget=MoneyMaskWidget(),
+    )
+    money3 = forms.DecimalField(
+        label="Money (widget)",
+        widget=MoneyMaskWidget(),
+        initial=Decimal("1234.9099992129"),
+        help_text="initial=1234.9099992129",
+    )
+    percent = forms.DecimalField(
+        label="Percent",
+        widget=PercentMaskWidget(),
+    )
